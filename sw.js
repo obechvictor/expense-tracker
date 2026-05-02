03f29f13f321dd668bdba93a7c0fe9c4c70345fd
Obech Victor Ouma <obechvictor0@gmail.com>
2026-05-02 09:55:02 +0300

    Upgrade service worker to use finance-v3 cache

--- sw.js
+++ sw.js
@@ -1,13 +1,28 @@
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open("finance-v2").then(cache=>{
-return cache.addAll(["/","/index.html"]);
-})
-);
+const CACHE = "finance-v3";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "./index.html"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-);
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
